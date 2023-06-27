931aa7579f472510b57daa2a211607f945f91dda
kk <kanbang@163.com>
2023-06-27 10:35:54 +0800

    vue3 ok

--- packages/baklavajs-plugin-options-vue/src/ButtonOption.vue
+++ packages/baklavajs-plugin-options-vue/src/ButtonOption.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Descripttion: 
+ * @version: 0.x
+ * @Author: zhai
+ * @Date: 2023-06-25 09:46:31
+ * @LastEditors: zhai
+ * @LastEditTime: 2023-06-26 08:42:34
+-->
 <template>
     <button @click="$emit('openSidebar')" class="dark-button --block">{{ name }}</button>
 </template>
@@ -6,12 +14,10 @@
 import { Options, Prop, Vue } from "vue-property-decorator";
 
 @Options({
-    emits: ["openSidebar"]
+    emits: ["openSidebar"],
 })
 export default class ButtonOption extends Vue {
-
     @Prop({ type: String })
     name!: string;
-
 }
 </script>
--- packages/baklavajs-plugin-options-vue/src/options.ts
+++ packages/baklavajs-plugin-options-vue/src/options.ts
@@ -1,11 +1,19 @@
-import ButtonOption from "./ButtonOption.vue";
-import CheckboxOption from "./CheckboxOption.vue";
-import InputOption from "./InputOption.vue";
-import IntegerOption from "./IntegerOption.vue";
-import NumberOption from "./NumberOption.vue";
-import SelectOption from "./SelectOption.vue";
-import SliderOption from "./SliderOption.vue";
-import TextOption from "./TextOption.vue";
+/*
+ * @Descripttion: 
+ * @version: 0.x
+ * @Author: zhai
+ * @Date: 2023-06-25 09:46:04
+ * @LastEditors: zhai
+ * @LastEditTime: 2023-06-26 09:04:40
+ */
+import  ButtonOption from "./ButtonOption.vue";
+import  CheckboxOption from "./CheckboxOption.vue";
+import  InputOption from "./InputOption.vue";
+import  IntegerOption from "./IntegerOption.vue";
+import  NumberOption from "./NumberOption.vue";
+import  SelectOption from "./SelectOption.vue";
+import  SliderOption from "./SliderOption.vue";
+import  TextOption from "./TextOption.vue";
 
 export {
     ButtonOption,
